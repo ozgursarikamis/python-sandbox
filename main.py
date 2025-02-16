efc0c6bfecc80e2d2bc5e7d61fdcc8f414fc13e2
Ozgur Sarikamis <ozgur.dev@icloud.com>
2025-02-17 01:31:19 +0300

    sqlalchemy & pyspark

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
 from pyspark.sql import SparkSession
+from sqlalchemy import create_engine
 
-spark = SparkSession.builder.appName("example").getOrCreate()
+import os
+from dotenv import load_dotenv
 
-data = [("John", 25),("Jane", 20), ("Mike", 20)]
+# Load environment variables from .env file
+load_dotenv()
 
-df = spark.createDataFrame(data, ["name", "age"])
-df.show()
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
 
-spark.stop()
+# CREATE AN ENGINE:
+engine = create_engine(f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
+
+try:
+    with engine.connect() as connection:
+        print('connection was successful')
+except Exception as e:
+    print(f"connection failed: {e}")
+
+# spark = SparkSession.builder.appName("example").getOrCreate()
+
+# data = [("John", 25),("Jane", 20), ("Mike", 20)]
+
+# df = spark.createDataFrame(data, ["name", "age"])
+# df.show()
+
+# spark.stop()
---
+++ read-data.py
@@ -0,0 +1,35 @@
+from pyspark.sql import SparkSession
+
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+PLSQL_JAR = os.getenv("PLSQL_JAR")
+
+# Initialize Spark session with the JDBC driver
+spark = SparkSession.builder \
+    .appName("PostgreSQL_Spark") \
+    .config("spark.jars", PLSQL_JAR) \
+    .getOrCreate()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+# Define PostgreSQL connection properties
+properties = {
+    "user": DB_USER,
+    "password": DB_PASSWORD,
+    "driver": "org.postgresql.Driver"
+}
+
+# JDBC URL format for PostgreSQL
+jdbc_url = f"jdbc:postgresql://{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+# Read data from PostgreSQL table into a Spark DataFrame
+df = spark.read.jdbc(url=jdbc_url, table='"public"."Organisations"', properties=properties)
+
+df.show()  # Display data
